3fb9f69d5ed5b0bc26222bedb98f8acd27cb6db7
Mohammed Noori <kmanoorib@gmail.com>
2018-05-25 00:00:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,16 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
+<<<<<<< HEAD
 
+=======
+=begin
+>>>>>>> 1344d329dd596f71bac4fbaa93e0f7bee8571361
   count = 1
   array.each do |name|
     puts "#{count}. #{name}"
     count+=1
   end
+<<<<<<< HEAD
 
   #commented code works the same as below...well it should(it doesn't)
 =begin
@@ -13,6 +18,14 @@ def roll_call_dwarves(array) # code an argument here
     puts "#{index+1}. #{name}"
   }
 =end
+=======
+=end
+  #commented code works the same as below
+  array.each_with_index |name, index| {
+    puts "#{index+1}. #{name}"
+  }
+  
+>>>>>>> 1344d329dd596f71bac4fbaa93e0f7bee8571361
 end
 
 def summon_captain_planet(array) # code an argument here
@@ -34,7 +47,7 @@ end
 def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |food|
+  array.detect do |food|
     cheese_types.include?(food)
   end
 end
